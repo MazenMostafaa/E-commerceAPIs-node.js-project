54d6805d6bc30d336cada92e76019b04077772c8
MazenMostafaa <MazenMostafa047@gmail.com>
2023-09-10 03:00:11 +0300

    Hide .env file and replace it with .env_sample

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from 'path'
 import { config } from 'dotenv'
 import { initiateApp } from './src/Utils/initiateApp.js'
 
-config({ path: path.resolve('./config/.env') })
+config({ path: path.resolve('.env') })
 
 const app = express()
 
